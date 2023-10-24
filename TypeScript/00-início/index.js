b038318623e702ede7fa3d4c605ccdd30d061589
Manoel <999142818m@gmail.com>
2023-10-24 10:36:39 -0300

    Init TS

---
+++ TypeScript/00-início/index.js
@@ -0,0 +1,4 @@
+var Pedido = function (pedido) {
+    return "pedido saiu para entrega ".concat(pedido);
+};
+console.log(Pedido('5345'));
---
+++ TypeScript/00-início/index.ts
@@ -0,0 +1,6 @@
+const Pedido = (pedido: string) => {
+    return `pedido saiu para entrega ${pedido}`
+}
+
+
+console.log(Pedido('5345'))
